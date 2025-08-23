af8ccdf97f0033541118a83aa44097e7a47d470b
Paul <p.salaun99@gmail.com>
2025-08-23 23:00:56 +0200

    👮 Fix analyze

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foreclair/src/data/providers/navigation_provider.dart';
-import 'package:foreclair/src/ui/pages/logbook/main/logbook_page.dart';
 import 'package:foreclair/src/ui/views/authentication/login_gateway.dart';
-import 'package:foreclair/src/ui/views/authentication/login_view.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
